892ff741fadf4c40180cc5602ebea35ce7bae2a7
Ricardo Damasceno <ricardodamascenno@gmail.com>
2017-10-01 18:50:27 -0300

    Addition and directory configuration of bower

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ var express = require('express');
 
 var app = express();
 
+//Number of the port
 var port = 5000;
+
+//Loads all static files inside the directory
+app.use(express.static('public'));
+app.use(express.static('src/views'));
 
 app.get('/',function(req,res){
     res.send('Hello World');
 });
 
+//Sets the port the application will listen
 app.listen(port, function(error){
     console.log('Server running on port ', port);
 });
