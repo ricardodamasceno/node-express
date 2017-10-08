54f5af6eeacb9e0a2e1193079bf7124886a5fd5e
Ricardo Damasceno <ricardodamascenno@gmail.com>
2017-10-08 13:05:07 -0300

    Addition and aconfiguration of Handlebars

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var handlebars = require('express-handlebars');
 
 var app = express();
 
@@ -7,10 +8,14 @@ var port = process.env.PORT || 5000;
 
 //Loads all static files inside the directory
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views','src/views');
+
+//Sets the template engine to Handlebars
+app.engine('.hbs', handlebars({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.render('index');
 });
 
 //Sets the port the application will listen
