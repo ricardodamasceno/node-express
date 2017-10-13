7962d348e0401de683ddbddfa709290defeab050
Ricardo Damasceno <ricardodamascenno@gmail.com>
2017-10-12 21:26:42 -0300

    Addition of books listing

--- app.js
+++ app.js
@@ -6,21 +6,65 @@ var app = express();
 //Number of the port
 var port = process.env.PORT || 5000;
 
+var bookRouter = express.Router();
+
+var books = [
+    {
+        title: 'Repurpose Content to Reach a Wider Audience',
+        site: '97thfloor.com'
+    },
+    {
+        title: '14 Useful Sites for Designers',
+        site: 'devgarage.com'
+    },
+    {
+        title: 'Measuring Your Link Building with Google Analytics',
+        site: 'searchenginewatch.com'
+    },
+    {
+        title: 'Dramatically Raise the Value of Any Piece of Content with These 27 Tactics',
+        site: 'searchenginewatch.com'
+    },
+    {
+        title: 'TrendPaper - Whats Trending in the World',
+        site: 'betali.st'
+    }
+];
+
 //Loads all static files inside the directory
 app.use(express.static('public'));
-app.set('views','src/views');
+app.set('views', 'src/views');
 
 //Sets the template engine to Handlebars
 app.engine('.hbs', handlebars({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
+bookRouter.route('/')
+    .get(function (req, res) {
+        res.render('books', {
+            title: 'Books',
+            nav: [
+                {link: 'Books', text: 'Books'},
+                {link: 'Authors', text: 'Authors'}
+            ],
+            books: books
+        });
+    });
+
+bookRouter.route('/detail')
+    .get(function (req, res) {
+        res.send('Hello Book details');
+    });
+
+app.use('/books', bookRouter);
+
 app.get('/', function (req, res) {
     res.render('index', {
         title: 'Hello from render',
         nav: [
-                {link: 'Books', text: 'Books'},
-                {link: 'Authors', text: 'Authors'}
-            ]
+            {link: 'Books', text: 'Books'},
+            {link: 'Authors', text: 'Authors'}
+        ]
     });
 });
 
