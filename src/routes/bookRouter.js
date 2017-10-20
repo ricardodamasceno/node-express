ba687560a31cced7d602c9dd2bb98f09c9de098b
Ricardo Damasceno <ricardodamascenno@gmail.com>
2017-10-20 20:13:11 -0200

    Bug fix on route

--- src/routes/bookRouter.js
+++ src/routes/bookRouter.js
@@ -9,7 +9,10 @@ var router = function (nav) {
     bookRouter.route('/')
         .get(function (req, res) {
             DBconnection.query('SELECT b.* FROM books b', function (error, results, fields) {
-                if (error) throw error;
+
+                if (error) {
+                    throw error;
+                }
 
                 res.render('booksListView', {
                     title: 'Books',
@@ -23,8 +26,10 @@ var router = function (nav) {
         .get(function (req, res) {
             var id = req.params.id;
 
-            DBconnection.query('SELECT b.* FROM books b where b.id = ' + id , function (error, results, fields) {
-                if (error) throw error;
+            DBconnection.query('SELECT b.* FROM books b where b.id = ' + id, function (error, results, fields) {
+                if (error) {
+                    throw error;
+                }
 
                 res.render('bookDetailView', {
                     title: 'Book Detail',
